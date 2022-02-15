89fe19d659525023459b0dcd53ddf96d70e985cd
emanuelecardone <e_m.cardone@yahoo.it>
2022-02-15 07:50:43 +0100

    Finito

---
+++ database/seeds/VoyagesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Voyage;
+use Faker\Generator as Faker;
+
+class VoyagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {   
+        for($i = 0; $i < 10; $i++){
+            //
+            $new_voyage = new Voyage();
+            $new_voyage->city = $faker->city();
+            $new_voyage->date = $faker->date();
+            $new_voyage->duration = rand(1, 7);
+            $new_voyage->save();
+        }
+    }
+}
